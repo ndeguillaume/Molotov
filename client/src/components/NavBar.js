bb77a9f4b37274c30a5dd7be0427cf548dab9fbe
Nicolas Deguillaume <nicolasdeguillaume@gmail.com>
2020-12-31 23:45:30 +0100

    MTN C GENRE INSANE POUR MODIF LES COULEURS C TROP BEAU

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -10,15 +10,32 @@ import pink from "./../public/images/pink.png";
 import yellow from "./../public/images/yellow.png";
 import red from "./../public/images/red.png";
 
+import FL_purple from "./../public/images/FL_purple.png";
+import FL_blue from "./../public/images/FL_blue.png";
+import FL_green from "./../public/images/FL_green.png";
+import FL_orange from "./../public/images/FL_orange.png";
+import FL_pink from "./../public/images/FL_pink.png";
+import FL_yellow from "./../public/images/FL_yellow.png";
+import FL_red from "./../public/images/FL_red.png";
+
 export default class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.colorIco = React.createRef();
-}
+  }
 
   render() {
     var color;
-    var colors = [purple, blue, green, orange, pink, yellow, red]
+    var colors = [purple, blue, green, orange, pink, yellow, red];
+    var flColors = [
+      FL_purple,
+      FL_blue,
+      FL_green,
+      FL_orange,
+      FL_pink,
+      FL_yellow,
+      FL_red,
+    ];
     if (this.props.color === "purple") color = purple;
     if (this.props.color === "blue") color = blue;
     if (this.props.color === "green") color = green;
@@ -44,8 +61,35 @@ export default class Navbar extends React.Component {
             handleClick={this.props.handleClick}
           />
         </div>
-        <i class="fas fa-paint-brush" ref={this.colorIco} onClick={this.props.colorClick}></i>
-        {this.props.displayColorDiv ? <ColorFilter setColor={this.props.setColor} colorIco={this.colorIco} colors={colors} /> : null}
+        {this.props.displayColorDiv ? (
+          <div className="change-color-wrapper focused">
+            {" "}
+            <i
+              class="fas fa-paint-brush"
+              ref={this.colorIco}
+              onClick={this.props.colorClick}
+            ></i>
+          </div>
+        ) : (
+          <div className="change-color-wrapper">
+            {" "}
+            <i
+              class="fas fa-paint-brush"
+              ref={this.colorIco}
+              onClick={this.props.colorClick}
+            ></i>
+          </div>
+        )}
+
+        {this.props.displayColorDiv ? (
+          <ColorFilter
+            color={this.props.color}
+            setColor={this.props.setColor}
+            colorIco={this.colorIco}
+            colors={colors}
+            flColors={flColors}
+          />
+        ) : null}
         <div className="button-wrapper">
           <div className="login-button">Login</div>
         </div>
