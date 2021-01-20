a2d0d6c9485acf16c2a5a3f75178588d78d4db8d
Fabien Alazet <32199307+FabienAlz@users.noreply.github.com>
2021-01-20 23:31:01 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 cp sample.env server/.env
 cp sample.env client/.env
 
-cd client
-gnome-terminal -- sudo npm start
-cd ../server
+cd server
 gnome-terminal -- sudo npm start 
+
+cd ../client
+gnome-terminal -- sudo npm start
